105d28c41247c8403daca42eb1a8bfb5e1ed482e
AlenaBelenets <alenaharenkova@gmail.com>
2022-09-30 18:27:35 +0300

    Added EntryVC and NewTaskVC

--- TaskList/ViewController.swift
+++ TaskList/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+//    @IBOutlet weak var tableView: UITableView!
+
+    var taskList = Task.getTaskList()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+    }
+
+    @IBAction func addNewTask() {
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+
+        return cell
+
+    }
+
+
+}
